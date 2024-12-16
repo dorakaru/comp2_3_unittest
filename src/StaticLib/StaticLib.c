c70f6d358db4736bade50d650c3def44ede1aa1f
dorakaru <dorakaru0909@gmail.com>
2024-12-16 14:51:13 +0900

    テストを通しました。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -21,5 +21,15 @@ int sum(int num, int* array)
 // num個の整数の平均を得る
 int average(int num, int* array)
 {
-	return 0x7fffffff;// ■未実装
+	int sum = 0;
+	float average = 0;
+
+	for (int i = 0; i < num; i++)
+	{
+		sum += array[i];
+	}
+
+	average = (float)(sum / num);
+
+	return average;// ■未実装
 }
